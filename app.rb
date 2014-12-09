7f1e5741ddf88e6b1177587416648700a95fab26
Christopher Beckett <chris.beckett@bigcommerce.com>
2014-12-10 10:23:32 +1100

    Adding swifts

--- app.rb
+++ app.rb
@@ -3,7 +3,19 @@ require 'json'
 
 swifts = [
 	"http://i.huffpost.com/gen/1856607/thumbs/o-TAYLOR-SWIFT-570.jpg",
-	"http://photos.laineygossip.com/lifestyle/taylor-swift-foundation-08dec1402.jpg",
+	"http://photos.laineygossip.com/lifestyle/taylor-swift-foundation-7dec1402.jpg",
+	"http://static.tvgcdn.net/MediaBin/Galleries/Editorial/140818/MTV_VMA_2014/2/smallcrops/vma-taylor-swift1sm.jpg",
+	"http://static1.businessinsider.com/image/52790bfd69beddf46041ccc2/taylor-swift-wrote-an-op-ed-in-the-wall-street-journal-and-its-filled-with-fascinating-insights.jpg",
+	"http://www.usmagazine.com/uploads/assets/photo_galleries/regular_galleries/2576-mtv-vmas-2014-red-carpet-beauty-breakdown-hair-makeup/photos/1408970942_taylor-swift-zoom.jpg",
+	"http://nyppagesix.files.wordpress.com/2013/12/taylor-swift.jpg",
+	"http://www.billboard.com/files/styles/promo_650/public/media/taylor-swift-shake-it-off-video-2-2014-billboard-650.jpg",
+	"http://static5.businessinsider.com/image/508916636bb3f7775400002d/how-taylor-swift-conquered-the-music-world-by-age-22.jpg",
+	"http://cbsradionews.files.wordpress.com/2014/10/taylor_swift_770_1.jpg%3Fw%3D620%26h%3D349%26crop%3D1",
+	"http://www.independent.ie/incoming/article30162439.ece/BINARY/483125117.jpg",
+	"http://cbsradionews.files.wordpress.com/2014/10/taylor_swift_770_1.jpg%3Fw%3D620%26h%3D349%26crop%3D1",
+	"http://blogs-images.forbes.com/jesscollen/files/2014/06/670px-taylor_swift_by_david_shankbone-e1401803550457.jpg",
+	"http://a.abcnews.com/images/Entertainment/HT_Taylor_Swift_ml_140818_16x9t_384.jpg",
+	"http://i.dailymail.co.uk/i/pix/2014/09/29/1412031442915_Image_galleryImage_125280_Taylor_Swift_seen_.JPG"
 ]
 
 get '/random' do
